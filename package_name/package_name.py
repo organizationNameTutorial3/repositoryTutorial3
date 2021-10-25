b46d433d745d36897485aa490110dac52c6de2c5
thalistdg <thalis_galeno@hotmail.com.br>
2021-10-25 16:15:41 -0300

    project created

---
+++ package_name/package_name.py
@@ -0,0 +1,10 @@
+def convert(name):
+	"""
+	Print something about converting notebook.
+	Args:
+		name (str): your name
+	Returns:
+		None
+	"""
+
+	print(name + " are learning to convert a notebook.")
